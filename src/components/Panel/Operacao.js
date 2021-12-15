62da2287f393d99f3388f12fe03d5017e73c1156
Andre gois <andre.s.gois3@gmail.com>
2021-12-14 21:09:53 -0300

    adding operations props

--- src/components/Panel/Operacao.js
+++ src/components/Panel/Operacao.js
@@ -9,21 +9,14 @@ const operacoes = [
 ];
 
 export default class Operacao extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      operacao: ''
-    }
-  }
-
   render(){
     return(
       <Picker 
         style={styles.Operacao}
         placeholder={{ label: "Selecione a operação", value: null }}
         items={operacoes}
-        onValueChange={ (value) => { this.setState({ operacao: value }); }}
-        value={this.state.operacao}
+        onValueChange={ (op) => { this.props.atualizaOperacao(op) }}
+        value={this.props.operacao}
       />
     );
   }
